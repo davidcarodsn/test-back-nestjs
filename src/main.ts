1f2e005727176bfc80a41a4c2db8daa1e7657e3a
David Caro <david@z2h.it>
2023-07-02 21:46:47 -0300

    Now yes allow everything

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin:'https://test-astro-dinamic.onrender.com',
+    origin:['https://test-astro-dinamic.onrender.com', 'http://localhost:3000'],
   });
   await app.listen(8080);
 }
